7a98d715cf98be2e298559ef0e8322c17794dd4d
Peter Gardner <wyregardner@icloud.com>
2023-02-09 14:47:53 +0000

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Peter Gardner on 09/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
